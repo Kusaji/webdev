520a548719c7fabdbde3c59d1e9917866fc34e7b
Zack Colbert <kusajiproduction@gmail.com>
2018-07-04 17:47:55 -0400

    July 4th Trump Soundboard Plus Portfolio Page

---
+++ pages/portfolio/trumpboard/scripts.js
@@ -0,0 +1,23 @@
+//All sounds for soundboard in a nice array.
+var sounds = [
+	new Audio('./sounds/makeAmericaGreatAgain.wav'),
+	new Audio('./sounds/china.wav'),
+	new Audio('./sounds/toughIsis.wav'),
+	new Audio('./sounds/aGreatWall.wav'),
+	new Audio('./sounds/greatGreatWall.wav'),
+	new Audio('./sounds/makeMexicoPay.wav'),
+	new Audio('./sounds/mexicoNewChina.wav'),
+	new Audio('./sounds/presidentObama.wav'),
+	new Audio('./sounds/beatMexico.wav'),
+	new Audio('./sounds/nicePerson.wav'),
+	new Audio('./sounds/rosieUgly.wav'),
+	new Audio('./sounds/freeTrade.wav')
+];
+
+function playSound(i) {
+	sounds[i].play();
+}
+
+function stopSound() {
+	stopSound();
+}

35c0b06a1a88ac55e3ebde188bfd1084b7ba98e5
Zack Colbert <kusajiproduction@gmail.com>
2018-07-05 18:11:13 -0400

    Added a stop button for the soundboard.
    Either press the stop button, or space bar to stop all sounds.

--- pages/portfolio/trumpboard/scripts.js
+++ pages/portfolio/trumpboard/scripts.js
@@ -18,6 +18,19 @@ function playSound(i) {
 	sounds[i].play();
 }
 
-function stopSound() {
-	stopSound();
+
+//This function will stop all sounds
+function stopSound(i) {
+	for (var i = 0; i < sounds.length; i++){
+	sounds[i].pause();
+	sounds[i].currentTime = 0;
+	}
 }
+//This function will cause the space bar to call the stopSound function
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 32:
+            stopSound();
+            break;
+    }
+};
